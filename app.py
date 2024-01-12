be6a95715f6b72b06e97b7a74a6fdb6a7a035019
Anshul Rawat <anshul.rawat1122004@gmail.com>
2024-01-12 15:35:25 +0530

    updated files for better ui

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import asyncio
 from flask import Flask, render_template, request
 from colorama import Fore, Style, init
 import socket
-from datetime import datetime
 import pyfiglet
 
 init(autoreset=True)  # Initialize colorama
@@ -9,42 +9,52 @@ init(autoreset=True)  # Initialize colorama
 app = Flask(__name__)
 
 
-def scan_port(target, port):
+async def scan_port(target, port):
     try:
-        with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
-            s.settimeout(1)
-            result = s.connect_ex((target, port))
-            if result == 0:
-                return f"{Fore.GREEN}Port {port} is open{Style.RESET_ALL}"
+        reader, writer = await asyncio.open_connection(target, port)
+        writer.close()
+        return f"{Fore.GREEN}Open{Style.RESET_ALL}"
     except (socket.gaierror, socket.error):
         pass  # Handle hostname resolution or server not responding errors
+    return f"{Fore.RED}Closed{Style.RESET_ALL}"
 
-    return None
 
+async def port_scan(target):
+    results = {}
 
-def port_scan(target, ipv6=False):
-    results = []
+    async def update_progress(port):
+        status = await scan_port(target, port)
+        results[port] = status
 
-    for port in range(1, 65536):
-        result = scan_port(target, port)
-        if result:
-            results.append(result)
+    tasks = [update_progress(port) for port in range(1, 1025)]
+    await asyncio.gather(*tasks)
 
-    return results
+    # Filter open ports
+    open_ports = {k: v for k, v in results.items() if "Open" in v}
+
+    return open_ports
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         target = request.form['target']
-        ip_version = request.form['ip_version']
-        results = port_scan(target, ipv6=(ip_version == '6'))
-        return render_template('index.html', target=target, ip_version=ip_version, results=results)
 
-    return render_template('index.html', target=None, ip_version=None, results=None)
+        print(f"Received POST request with target: {target}")
+
+        # Perform the port scanning operation
+        results = asyncio.run(port_scan(target))
+
+        print("Results:")
+        for port, status in results.items():
+            print(f"Port {port}: {status}")
+
+        return render_template('results.html', target=target, results=results)
+
+    return render_template('index.html', target=None, results=None)
 
 
 if __name__ == '__main__':
     ascii_banner = pyfiglet.figlet_format("PORT SCANNER", font="slant")
     print(f"{Fore.CYAN}{ascii_banner}{Style.RESET_ALL}")
-    app.run(debug=True, threaded=False)
+    app.run(debug=True, threaded=True)
